12d0e097df614c1c686e1ecd109d41412641f8b6
ryanburgess <ryanb@netflix.com>
2015-10-04 19:32:06 -0700

    remove updates to day

--- jsx/day.jsx
+++ jsx/day.jsx
@@ -1,35 +1,7 @@
 var React = require('react');
 
-var SetIntervalMixin = {
-  componentWillMount: function componentWillMount() {
-    this.intervals = [];
-  },
-
-  componentWillUnmount: function componentWillUnmount() {
-    this.intervals.map(clearInterval);
-  },
-
-  setInterval: (function (_setInterval) {
-    function setInterval() {
-      return _setInterval.apply(this, arguments);
-    }
-
-    setInterval.toString = function () {
-      return _setInterval.toString();
-    };
-
-    return setInterval;
-  })(function () {
-    this.intervals.push(setInterval.apply(null, arguments));
-  })
-};
-
 var Day = React.createClass({
-  mixins: [SetIntervalMixin],
-  componentDidMount: function componentDidMount() {
-    this.setInterval(this.tick, 1000);
-  },
-  tick: function tick() {
+  render: function() {
     let d = new Date();
     let weekday = new Array(7);
     weekday[0]=  'Sunday';
@@ -41,10 +13,6 @@ var Day = React.createClass({
     weekday[6] = 'Saturday';
 
     let day = weekday[d.getDay()];
-    this.setState({ day: day });
-  },
-  render: function() {
-    
     return (
       <p className='day'>{day}</p>
     );
