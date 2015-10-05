014ef79475a677dd25a047578d902b8814c7b83a
ryanburgess <ryanb@netflix.com>
2015-10-04 19:19:01 -0700

    fix day updating

--- jsx/day.jsx
+++ jsx/day.jsx
@@ -1,7 +1,35 @@
 var React = require('react');
 
+var SetIntervalMixin = {
+  componentWillMount: function componentWillMount() {
+    this.intervals = [];
+  },
+
+  componentWillUnmount: function componentWillUnmount() {
+    this.intervals.map(clearInterval);
+  },
+
+  setInterval: (function (_setInterval) {
+    function setInterval() {
+      return _setInterval.apply(this, arguments);
+    }
+
+    setInterval.toString = function () {
+      return _setInterval.toString();
+    };
+
+    return setInterval;
+  })(function () {
+    this.intervals.push(setInterval.apply(null, arguments));
+  })
+};
+
 var Day = React.createClass({
-  render: function() {
+  mixins: [SetIntervalMixin],
+  componentDidMount: function componentDidMount() {
+    this.setInterval(this.tick, 1000);
+  },
+  tick: function tick() {
     let d = new Date();
     let weekday = new Array(7);
     weekday[0]=  'Sunday';
@@ -13,6 +41,10 @@ var Day = React.createClass({
     weekday[6] = 'Saturday';
 
     let day = weekday[d.getDay()];
+    this.setState({ day: day });
+  },
+  render: function() {
+    
     return (
       <p className='day'>{day}</p>
     );
